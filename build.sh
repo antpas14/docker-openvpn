d43b1c4e9480cb4042cac489f4443bbb46913434
Antonello Pastena <antonello.pastena@gmail.com>
2019-05-01 19:39:04 +0200

    added build script

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# This script builds the Dockerfile file for current system architecture and pushs it to a dockerhub 
+
+REPO_PATH="antpas14/openvpn"
+
+main() {
+	build_image
+}
+
+build_image() {
+	DOCKERFILE = "./Dockerfile-"
+	ARCH=$( get_current_arch )
+	docker build -t ${DOCKERFILE}${ARCH} $REPO_PATH:$ARCH
+	docker push $REPO_PATH:$ARCH
+}
+
+get_current_arch() {
+	uname -m
+}
+
+main $@
